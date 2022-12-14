78c1cba8d284f0d5d602d9f8aa02885ea52d0438
Gary Smith <garysmith193358@gmail.com>
2022-12-14 10:19:50 -0500

    implemented mongodb with user collection

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,19 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import User from './models/User';
+
 const app: Application = express();
 const PORT = 8080;
+
+const syncAndSeed = async() => {
+  mongoose.connect("mongodb://localhost/appdb")
+  const user = new User({name: "Jackie", age: 25})
+  await user.save()
+  console.log("user saved", user)
+}
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
+
+syncAndSeed();
